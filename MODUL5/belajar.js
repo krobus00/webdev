07bf854494752776ed56978749301797564fe015
krobus00 <michaelglaxy@gmail.com>
2020-01-31 20:46:02 +0700

    modul 5

---
+++ MODUL5/belajar.js
@@ -0,0 +1,61 @@
+// Cara deklarasi variabel
+// cara pertama
+var variabel1 = "17"
+
+// cara kedua 
+let variabel2 = "17"
+
+// cara ketiga
+variabel3 = "17"
+
+// cara penulisan fungsi
+// cara 1
+function fungsi1(){
+    // do nothing
+    return 0
+}
+
+// cara 2
+var variabel4 = function(a,b){
+    //do nothing
+    return 0
+}
+
+// cara 3
+var variabel5 = (a,b) => {
+    // do nothing
+    return 0
+}
+
+
+// percabangan
+// operator ?
+x = 17
+x=17 ? 'true' : 'false'
+
+// if else
+if (x=1) {
+    // do nothing
+}else if (x=1) {
+    // do nothing
+} else {
+    // do nothing
+}
+
+// perulangan
+arr = [1,2,3,4,5,6,7,8,9,10]
+for(let i = 0;i<10;i++){
+    // do nothing
+}
+
+for(item in arr){
+    console.log(item)
+}
+
+while (x < 17) {
+    x++
+}
+do{
+    // do nothing
+    x++
+}while(x < 17)
