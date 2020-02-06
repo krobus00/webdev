7832cd732440cf991594362bd8d3f39bd3bc999f
krobus00 <michaelglaxy@gmail.com>
2020-02-06 15:13:33 +0700

    penambahan console log

--- MODUL5/belajar.js
+++ MODUL5/belajar.js
@@ -10,19 +10,19 @@ variabel3 = "17"
 
 // cara penulisan fungsi
 // cara 1
-function fungsi1(){
+function fungsi1() {
     // do nothing
     return 0
 }
 
 // cara 2
-var variabel4 = function(a,b){
+var variabel4 = function(a, b) {
     //do nothing
     return 0
 }
 
 // cara 3
-var variabel5 = (a,b) => {
+var variabel5 = (a, b) => {
     // do nothing
     return 0
 }
@@ -31,31 +31,33 @@ var variabel5 = (a,b) => {
 // percabangan
 // operator ?
 x = 17
-x=17 ? 'true' : 'false'
+x = 17 ? 'true' : 'false'
 
 // if else
-if (x=1) {
+if (x = 1) {
     // do nothing
-}else if (x=1) {
+} else if (x = 1) {
     // do nothing
 } else {
     // do nothing
 }
 
 // perulangan
-arr = [1,2,3,4,5,6,7,8,9,10]
-for(let i = 0;i<10;i++){
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for (let i = 0; i < 10; i++) {
     // do nothing
 }
 
-for(item in arr){
+for (item in arr) {
     console.log(item)
 }
 
 while (x < 17) {
+    console.log(x)
     x++
 }
-do{
+do {
+
     // do nothing
     x++
-}while(x < 17)
+} while (x < 17)
